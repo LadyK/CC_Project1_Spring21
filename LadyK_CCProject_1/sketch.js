69c218d816e37ad87af50e55be7be011e4a44338
LadyK <LadyK@users.noreply.github.com>
2021-03-16 14:53:59 -0400

    added background and graphic

--- LadyK_CCProject_1/sketch.js
+++ LadyK_CCProject_1/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
   // put setup code here
+  createCanvas(400, 400);
+  background(0);
 }
 
 function draw() {
   // put drawing code here
+  fill(255);
+  ellipse(width/2, height/2, 50, 50);
 }
